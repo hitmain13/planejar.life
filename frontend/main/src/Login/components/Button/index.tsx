1a0e4984314e631b3c0867bbbb1ecfc055c7a9d4
Fabio Matsumoto <fabio.matsumoto@br.experian.com>
2023-10-25 00:58:52 -0300

    chore: definição de configuração inicial do projeto: eslint main, tsconfig.jest, vscode settings

--- frontend/main/src/Login/components/Button/index.tsx
+++ frontend/main/src/Login/components/Button/index.tsx
@@ -1,6 +1,1 @@
-import React from 'react';
-
-
-export const Button = ({ label } : { label?: string }) => {
-  return <button>{label}</button>;
-};
+export const Button = ({ label } : { label?: string }) => <button>{label}</button>
